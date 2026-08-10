9f15b684155c357db97b9b7b2892e9c0dd4ec36d
bechir300 <64028401+bechir300@users.noreply.github.com>
2026-08-10 23:26:08 +0100

    Update version comment in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // ============================================================
-// MONDECO - AGENT WHATSAPP + INSTAGRAM + FACEBOOK + COMMENTAIRES + IA + RESPONSABLE COMMERCIAL + SLA — V6.27.2
+// MONDECO - AGENT WHATSAPP + INSTAGRAM + FACEBOOK + COMMENTAIRES + IA + RESPONSABLE COMMERCIAL + SLA — V6.27.3
 // server.js
 //
 // Ajouts V5 :
