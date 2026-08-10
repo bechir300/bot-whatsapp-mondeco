621dcf4cc16afe30380eb9e3f93b87f311506866
bechir300 <64028401+bechir300@users.noreply.github.com>
2026-08-10 15:23:29 +0100

    Update version to V6.20.7 in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // ============================================================
-// MONDECO - AGENT WHATSAPP + INSTAGRAM + FACEBOOK + IA + RESPONSABLE COMMERCIAL + SLA — V6.20.6
+// MONDECO - AGENT WHATSAPP + INSTAGRAM + FACEBOOK + IA + RESPONSABLE COMMERCIAL + SLA — V6.20.7
 // server.js
 //
 // Ajouts V5 :
@@ -274,7 +274,7 @@ console.log('');
 console.log(
   '=============================================='
 );
-console.log('🚀 MONDECO OMNICANAL WHATSAPP + INSTAGRAM + FACEBOOK V6.20.6');
+console.log('🚀 MONDECO OMNICANAL WHATSAPP + INSTAGRAM + FACEBOOK V6.20.7');
 console.log(
   '=============================================='
 );
